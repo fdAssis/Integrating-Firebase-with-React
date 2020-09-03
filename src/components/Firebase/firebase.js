621b19597b608e4fb92cd9846825e720f92efcfe
fdAssis <fd07ass@gmail.com>
2020-09-03 16:46:55 -0300

    Firebase's authentication API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,6 @@
-import app from 'firebase/app'
+import app from 'firebase/app';
+import 'firebase/auth';
+
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,7 +15,27 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+
+  // Sign-Up
+  doCreateUserWithEmailAndPassword = (email, password) => 
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  // Sign-In
+  doSignInWithEmailAndPassword = (email, password) => 
+    this.auth.signInWithEmailAndPassword(email, password);  
+
+  doSignOut = () => this.auth.signOut();
+
+  // Reset/Change Password
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => 
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
